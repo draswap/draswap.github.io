6a7c476a0cd128d62b407f46201df843c267ec23
drasup <support@draswap.com>
2024-08-19 22:43:05 +0800

    tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'draSwap',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'draSwap Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
