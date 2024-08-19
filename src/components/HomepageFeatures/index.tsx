90ec15d3dade04d5714b6efae5ebc0ecc7e3f06e
drasup <support@draswap.com>
2024-08-20 07:40:08 +0800

    Index Feature

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,35 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Everyone as a Market Maker',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        DdraSwap uses a fair and scientific approach to calculations, 
+        allowing everyone to participate in adding liquidity 
+        and earn tokens without incurring impermanent loss.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Commercial Ecosystem',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        We provide technical support for any commercial payment 
+        application system platform community, including Token, 
+        Dapp, DeFi, NFT, <code>DEX</code>, blockchain games, and wallets.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Security and Privacy',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        draSwap is implemented based on EVM technology, offering complete
+        secure transactions, tamper-proof on-chain records, payment 
+        authentication, and future functionalities.
       </>
     ),
   },
