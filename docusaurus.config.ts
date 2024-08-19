021b9de4b69b2ff9d877fd2cf2ce488a1a0373ed
drasup <support@draswap.com>
2024-08-20 07:25:38 +0800

    themeConfig Git

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/draswap',
           label: 'GitHub',
           position: 'right',
         },
@@ -95,7 +95,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Docs',
               to: '/docs/intro',
             },
           ],
